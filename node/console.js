f18cde9cb2c7384eb1e8004e3ff58b8165dd66e4
David Vieira <davidvieira0110@gmail.com>
2021-04-27 00:21:36 -0300

    Aprendendo um poucod de OOJS

--- node/console.js
+++ node/console.js
@@ -1,1 +1,25 @@
-console.log('Ola');
+function Person(first, last, age, gender, interests) {
+    this.name = {
+        'first': first,
+        'last': last
+    };
+    this.age = age;
+
+    this.gender = gender;
+    this.interests = interests;
+    this.bio = function () {
+        this.gender === "male" ? console.log(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.') :
+            console.log(this.name.first + ' ' + this.name.last + ' is ' + this.age + ' years old. She likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
+
+
+    };
+    this.greeting = function () {
+        alert('Hi! I\'m ' + this.name.first + '.');
+    };
+}
+
+// var me = new Person("David", "Vieira", 18, "female", ["Progamation", "Academy"])
+
+// me.bio()
+
+var person = new Object()
